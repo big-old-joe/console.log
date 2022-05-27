20181e1b81c480380c9144a4bca5946802c68fb8
big-old-joe <97917645+big-old-joe@users.noreply.github.com>
2022-05-27 20:50:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my first console project");
+
+var first_name="Ayush";
+var last_name="Arora";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=10;
+var b=5;
+
+var add= a+b;
+console.log(add);
+
+var subtraction= a-b;
+console.log(subtraction);
+
+var multiplication= a*b;
+console.log(multiplication);
+
+var division= a/b;
+console.log(division);
